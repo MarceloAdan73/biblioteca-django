84b2c24cabd29690daa0888339b2c76afc9faa2e
Marcelo <marcelo24mp@gmail>
2025-11-06 02:02:34 -0300

    Fix: complete logout solution and static files configuration

--- biblioteca_project/settings.py
+++ biblioteca_project/settings.py
@@ -99,3 +99,9 @@ if 'RENDER' in os.environ:
         )
     }
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configuración de archivos estáticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
